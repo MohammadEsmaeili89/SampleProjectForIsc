620c773186fc1fc3a218319598728e1254ea4a34
Esmaeili Mohammad <M_Esmaeili@ISC.CO.IR>
2023-10-28 14:43:09 +0330

    edit

--- src/main/java/com/ics/esmaeili/homeTask/controller/TaskController.java
+++ src/main/java/com/ics/esmaeili/homeTask/controller/TaskController.java
@@ -19,26 +19,26 @@ public class TaskController {
         return ResponseEntity.ok(taskService.save(taskDto));
     }
 
-//    @GetMapping("/{taskId}")
-//    ResponseEntity<TaskDto> findById(@PathVariable("taskId") Integer id) {
-//        return ResponseEntity.ok(taskService.findById(id));
-//    }
-//
-//    @GetMapping
-//    ResponseEntity<Page<TaskDto>> findAll(@RequestParam("pageNumber") Integer pageNumber , @RequestParam("pageSize") Integer pageSize) {
-//        return ResponseEntity.ok(taskService.findAll(pageNumber,pageSize));
-//    }
-//
-//    @PutMapping("/{taskId}")
-//    ResponseEntity<TaskDto> update(@Validated @RequestBody TaskDto taskDto,@PathVariable("taskId") Integer id){
-//        taskDto.setId(id);
-//        return ResponseEntity.ok(taskService.save(taskDto));
-//    }
-//
-//    @DeleteMapping("/{taskId}")
-//    ResponseEntity deleteById(@PathVariable("taskId") Integer id) {
-//        taskService.delete(id);
-//        return ResponseEntity.ok().build();
-//    }
+    @GetMapping("/{taskId}")
+    ResponseEntity<TaskDto> findById(@PathVariable("taskId") Integer id) {
+        return ResponseEntity.ok(taskService.findById(id));
+    }
+
+    @GetMapping
+    ResponseEntity<Page<TaskDto>> findAll(@RequestParam("pageNumber") Integer pageNumber , @RequestParam("pageSize") Integer pageSize) {
+        return ResponseEntity.ok(taskService.findAll(pageNumber,pageSize));
+    }
+
+    @PutMapping("/{taskId}")
+    ResponseEntity<TaskDto> update(@Validated @RequestBody TaskDto taskDto,@PathVariable("taskId") Integer id){
+        taskDto.setId(id);
+        return ResponseEntity.ok(taskService.save(taskDto));
+    }
+
+    @DeleteMapping("/{taskId}")
+    ResponseEntity deleteById(@PathVariable("taskId") Integer id) {
+        taskService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
